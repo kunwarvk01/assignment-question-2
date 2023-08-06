c4f3b72ddad84e16d27d4a1a48b1427c683cbbe4
kunwarvk01 <kunwarvk01@gmail.com>
2023-08-06 23:07:43 +0530

    configured code to run sample testcase

--- optimal.js
+++ optimal.js
@@ -19,16 +19,16 @@ function handleOverlapsAndNestedPositions(plainTextPositions) {
 
 function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
   // checking for invalid inputs
-  if (
-    !htmlContent ||
-    !plainText ||
-    !plainTextPositions ||
-    plainTextPositions.length == 0 ||
-    !isNaN(htmlContent) ||
-    !isNaN(plainText)
-  ) {
-    return htmlContent;
-  }
+  // if (
+  //   !htmlContent ||
+  //   !plainText ||
+  //   !plainTextPositions ||
+  //   plainTextPositions.length == 0 ||
+  //   !isNaN(htmlContent) ||
+  //   !isNaN(plainText)
+  // ) {
+  //   return htmlContent;
+  // }
 
   const openingTag = "<mark>";
   const closingTag = "</mark>";
@@ -41,21 +41,34 @@ function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
     handleOverlapsAndNestedPositions(plainTextPositions);
 
   while (i < htmlContent.length) {
-    if (
-      z < resolvedPositions.length &&
-      resolvedPositions[z].start == j &&
-      htmlContent[i] == plainText[j]
-    )
-      outputHTML += openingTag;
+    if (htmlContent[i] == "<") {
+      while (true) {
+        outputHTML += htmlContent[i];
+        i++;
+        if (htmlContent[i] == ">") {
+          outputHTML += htmlContent[i]; // Add the closing angle bracket to the output
+          break;
+        }
+      }
+    } else if (z < resolvedPositions.length) {
+      if (htmlContent[i - 2] == "r" && htmlContent[i - 3] == "b") j++;
+      if (j == resolvedPositions[z].start && htmlContent[i] === plainText[j]) {
+        outputHTML += openingTag;
+      }
 
-    outputHTML += htmlContent[i];
+      outputHTML += htmlContent[i];
 
-    if (z < resolvedPositions.length && resolvedPositions[z].end == j + 1) {
-      outputHTML += closingTag;
-      z++;
+      if (
+        j + 1 == resolvedPositions[z].end &&
+        htmlContent[i] === plainText[j]
+      ) {
+        outputHTML += closingTag;
+        z++;
+      }
+
+      j++;
     }
 
-    if (htmlContent[i] === plainText[j]) j++;
     i++;
   }
 
@@ -73,13 +86,6 @@ function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
 //       { start: 12, end: 16 },
 //     ]
 //   )
-// );
-
-// console.log(
-//   highlightHTMLContent("<p>HTML content</p>", "HTML content", [
-//     { start: 0, end: 4 },
-//     { start: 1, end: 12 },
-//   ])
 // );
 
 // console.log(
@@ -116,6 +122,17 @@ function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
 //       { start: 12, end: 17 },
 //       { start: 25, end: 29 },
 //       { start: 38, end: 42 },
+//     ]
+//   )
+// );
+
+// console.log(
+//   highlightHTMLContent(
+//     '<p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects<br><br>Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar…<br><br>Read the full article <a href="https://content.seleritycorp.com/hosted/assets/www/UKMW47_hYz_RGzPSpHm44Hi1L49HdNBhs1OkKKW2OPI">here</a><br><br>-------------------------------------<br><br>You received this because you are subscribed to news related to <a href="https://iris.steeleye.co/market/instruments?search=ES0113900J37">ES0113900J37</a>, and this story was marked as 82% relevant.<br><br>Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. <br><br>To unsubscribe change your email preferences, please click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">here</a>.<br><br>-------------------------------------<br><br><img src="https://context.seleritycorp.com/selerity/assets/sc_icons/pressRelease.png" alt="Rick Astley" style="width:100px;height:100px;"></span></p>',
+//     "Hi David Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar… Read the full article here ------------------------------------- You received this because you are subscribed to news related to ES0113900J37 , and this story was marked as 82% relevant. Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. To unsubscribe change your email preferences, please click here . -------------------------------------",
+//     [
+//       { start: 241, end: 247 },
+//       { start: 518, end: 525 },
 //     ]
 //   )
 // );
